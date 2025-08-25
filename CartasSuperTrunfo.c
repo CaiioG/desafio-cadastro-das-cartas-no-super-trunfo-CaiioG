ad3891114b7983d6cc0b68285729c631497d6972
Caio Gomes <caio.luis103@gmail.com>
2025-08-25 22:03:58 +0000

    Adicionando o PIb e a Dencidade populacional, colocando o codigo para somar ambos. deixando o codigo funcional para a batalha de verdade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,59 @@
 #include <stdio.h>
+#include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Definindo a estrutura para a Carta
+struct Carta {
+    char Estado[20];
+    char Codigo_da_carta[4];
+    char Nome_do_cidade[20];
+    int Populacao;
+    float Area_em_km2;
+    float Pib;
+    int Numero_de_pontos_turisticos;
+};
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    //Criando duas variáveis para as cartas
+    struct Carta carta1;
+    struct Carta carta2;
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    //Atribuir os valores para a carta N°1
+    strcpy(carta1.Estado, "Brasilia");
+    strcpy(carta1.Codigo_da_carta, "B01");
+    strcpy(carta1.Nome_do_cidade, "Distrito Federal");
+    carta1.Populacao = 2980000;
+    carta1.Area_em_km2 = 5802.0;
+    carta1.Pib = 286900.0;
+    carta1.Numero_de_pontos_turisticos = 4;
+
+    //Atribuir os valores para a carta N°2
+    strcpy(carta2.Estado, "Rio de Janeiro");
+    strcpy(carta2.Codigo_da_carta, "B02");
+    strcpy(carta2.Nome_do_cidade, "Saquarema");
+    carta2.Populacao = 89600;
+    carta2.Area_em_km2 = 352.0;
+    carta2.Pib = 42000.0;
+    carta2.Numero_de_pontos_turisticos = 4;
+
+    // Saida dos valores carta N°1
+    printf("Brasilia\n");
+    printf("Estado: %s\n", carta1.Estado);
+    printf("Codigo da Carta: %s\n", carta1.Codigo_da_carta);
+    printf("Cidade: %s\n", carta1.Nome_do_cidade);
+    printf("Populacao: %d\n", carta1.Populacao);
+    printf("Area em km2: %.2f\n", carta1.Area_em_km2);
+    printf("PIB: %.2f\n", carta1.Pib);
+    printf("Pontos Turisticos: %d\n", carta1.Numero_de_pontos_turisticos);
+
+    // Saida dos valores carta N°2
+    printf("\nRio de Janeiro\n");
+    printf("Estado: %s\n", carta2.Estado);
+    printf("Codigo da Carta: %s\n", carta2.Codigo_da_carta);
+    printf("Cidade: %s\n", carta2.Nome_do_cidade);
+    printf("Populacao: %d\n", carta2.Populacao);
+    printf("Area em km2: %.2f\n", carta2.Area_em_km2);
+    printf("PIB: %.2f\n", carta2.Pib);
+    printf("Pontos Turisticos: %d\n", carta2.Numero_de_pontos_turisticos);
 
     return 0;
 }
